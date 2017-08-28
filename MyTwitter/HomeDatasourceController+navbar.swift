80eea328ccc4df2c39988ea2b2cee2c64352ade2
egorchernenko <lordo916@gmail.com>
2017-08-28 22:02:39 +0300

    Custom Navigation Bar;

---
+++ MyTwitter/HomeDatasourceController+navbar.swift
@@ -0,0 +1,50 @@
+//
+//  HomeDatasourceController+navbar.swift
+//  MyTwitter
+//
+//  Created by Egor on 28.08.17.
+//  Copyright © 2017 Egor. All rights reserved.
+//
+
+import UIKit
+
+extension HomeDatasourceController{
+    func setupNavigationBarItems(){
+        setupRemainingNavItems()
+        setupLeftNavItems()
+        setuptRightNavItems()
+    }
+    
+    private func setupRemainingNavItems(){
+        let titleImageView = UIImageView(image: #imageLiteral(resourceName: "title_icon"))
+        titleImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        titleImageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = titleImageView
+        
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+    }
+    
+    private func setupLeftNavItems(){
+        let followButton = UIButton(type: .system)
+        followButton.setImage(#imageLiteral(resourceName: "follow").withRenderingMode(.alwaysOriginal), for: .normal)
+        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+    }
+    
+    private func setuptRightNavItems(){
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "search").withRenderingMode(.alwaysOriginal), for: .normal)
+        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "compose").withRenderingMode(.alwaysOriginal), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        
+        
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton),UIBarButtonItem(customView: searchButton)]
+    }
+}
+
