f0c8920ccd4803acc1ac7141d03e6ac82e0c37a8
egorchernenko <lordo916@gmail.com>
2017-08-30 11:45:55 +0300

    Tweets String Formatting Tips And Tricks with NSAttributedString;

--- MyTwitter/HomeDatasource.swift
+++ MyTwitter/HomeDatasource.swift
@@ -20,7 +20,13 @@ class HomeDatasource: Datasource {
     }()
     
     //let words = ["user1","user2","user3"]
-    let tweets = ["tweet1", "tweet2"]
+    let tweets: [Tweet] = {
+        let elonUser = User(name: "Elon Musk ", username: "@elonmusk", bioText: "Pics of SpaceX spacesuit developed for NASA commercial crew program coming out next week. Undergoing ocean landing mobility/safety tests.",profileImage: #imageLiteral(resourceName: "profile_image"))
+
+        let tweet = Tweet(user: elonUser, message: "Welcome to nine step of my awesome app, relly you hope guye are you enjoy this stuff dddsdfsdfsdfsfsmsfjfksljfsjdkfsdklfkl;sdjdlksdjflkasdjfalsdjfasdlkfjsdklfjsdf;lkjaslajfalskjfadslfk;asjflkasdjflkasdjflaksfjasldkfjaslkfjasdlk;fjalaksjflkasfjeio;dklsfjasdokfl;sdjflkaqwoaksljdfmcao;skldfzc")
+        let tweet2 = Tweet(user: elonUser, message: "Welcome to nine step of my awesome app, relly you hope guye are you enjoy this stuff")
+        return [tweet,tweet2]
+    }()
     
     override func footerClasses() -> [DatasourceCell.Type]? {
         return [UserFooter.self]
@@ -35,6 +41,9 @@ class HomeDatasource: Datasource {
     }
     
     override func item(_ indexPath: IndexPath) -> Any? {
+        if indexPath.section == 1{
+            return tweets[indexPath.item]
+        }
         return users[indexPath.item]
     }
     
---
+++ MyTwitter/Tweet.swift
@@ -0,0 +1,14 @@
+//
+//  Tweet.swift
+//  MyTwitter
+//
+//  Created by Egor on 30.08.17.
+//  Copyright © 2017 Egor. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+    let user: User
+    let message: String
+}
