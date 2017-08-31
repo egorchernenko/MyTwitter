bd0cc74ea48b24ee598fb3c72bb0c7eca2815dee
egorchernenko <lordo916@gmail.com>
2017-08-31 15:30:03 +0300

    Easy Caching and Loading Images using CachedImageView;

--- MyTwitter/HomeDatasourceController.swift
+++ MyTwitter/HomeDatasourceController.swift
@@ -59,18 +59,32 @@ class HomeDatasourceController : DatasourceController{
     
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        if let user = self.datasource?.item(indexPath) as? User{
+        if indexPath.section == 0{
+            guard let user = datasource?.item(indexPath) as? User else {return .zero}
             
-            let approximateWidthOfTextView = view.frame.width - 12 - 50 - 12 - 2
-            let size = CGSize(width: approximateWidthOfTextView, height: 1000)
-            let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            let estimatedHeight = estimatedHeightForText(user.bioText)
             
-            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+            return CGSize(width: view.frame.width, height: estimatedHeight + 66)
             
-            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        } else if indexPath.section == 1{
+            guard let tweet = datasource?.item(indexPath) as? Tweet else {return .zero}
+            
+            let estimatedHeight = estimatedHeightForText(tweet.message)
+            
+            return CGSize(width: view.frame.width, height: estimatedHeight + 74)
         }
         
         return CGSize(width: view.frame.width, height: 200)
+    }
+    
+    private func estimatedHeightForText(_ text: String) -> CGFloat{
+        let approximateWidthOfTextView = view.frame.width - 12 - 50 - 12 - 2
+        let size = CGSize(width: approximateWidthOfTextView, height: 1000)
+        let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+        
+        let estimatedFrame = NSString(string: text).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+        
+        return estimatedFrame.height
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
